0c62ec47cfe5fe7ccafc7f491cb881c2f93615be
sothbak <61996130+sothbak@users.noreply.github.com>
2020-04-21 11:10:28 +1000

    changed app.js

--- controllers/authorController.js
+++ controllers/authorController.js
@@ -1,27 +1,43 @@
-// provide the controller a link to the author model
-var authors = require('../models/author');
-// Function to handle a request to get all authors
-const getAllAuthors = (req, res) => {
-    res.send(authors); // return the list of authors
+const mongoose = require("mongoose");
+
+// import author model
+const Author = mongoose.model("author");
+
+    
+// function to handle a request to get all authors
+const getAllAuthors = async (req, res) => {
+    
+  try {
+    const all_authors = await Author.find();
+    return res.send(all_authors);
+  } catch (err) {
+    res.status(400);
+    return res.send("Database query failed");
+  }
+};
+    
+  
+  
+
+// function to modify author by ID
+const updateAuthor = async (req, res) => {
+  res.send("Working on this feature");
 };
 
-// function to handle a request to a particular author
+// function to add author
+const addAuthor = async (req, res) => {
+ res.send("Working on this feature");
+};
+
+// function to get author by id
 const getAuthorByID = (req, res) => {
-    // search for author in the database via ID
-    const author = authors.find(author => author.id === req.params.id);
-
-    if (author) {
-        // send back the author details
-        res.send(author);
-    } else {
-        // you can decide what to return if author is not found
-        // currently, an empty list will be returned
-        res.send([]);
-    }
+  res.send("Working on this feature");
 };
 
-// Remember to export the callbacks
+// remember to export the functions
 module.exports = {
-    getAllAuthors,
-    getAuthorByID
+  getAllAuthors,
+  getAuthorByID,
+  addAuthor,
+  updateAuthor
 };
